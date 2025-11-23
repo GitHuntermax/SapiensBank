cc52759183152ed62bc37a18ac9a84185d83ed1d
GitHuntermax <macrog0078@gmail.com>
2025-11-23 13:47:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿var banco = new Banco();
-var ux = new UX("Sapiens Bank", banco);
-ux.Executar();
+using System;
+
+namespace SapiensBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco = new Banco();
+            UX ux = new UX("Sapiens Bank", banco);
+            ux.Executar();
+        }
+    }
+}
