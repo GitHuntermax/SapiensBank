7b4675369a104db2029a7e68ed1072a8a7d6913a
GitHuntermax <macrog0078@gmail.com>
2025-11-23 13:47:16 -0300

    Update Conta.cs

--- Conta.cs
+++ Conta.cs
@@ -1,23 +1,24 @@
-﻿using System.Text.Json.Serialization;
-
-public class Conta
+namespace SapiensBank
 {
-    public int Numero { get; set; }
-    public string Cliente { get; set; }
-    public string Cpf { get; set; }
-    public string Senha { get; set; }
-    public decimal Saldo { get; set; }
-    public decimal Limite { get; set; }
-
-    [JsonIgnore]
-    public decimal SaldoDisponível => Saldo + Limite;
-
-    public Conta(int numero, string cliente, string cpf, string senha, decimal limite = 0)
+    public class Conta
     {
-        Numero = numero;
-        Cliente = cliente;
-        Cpf = cpf;
-        Senha = senha;
-        Limite = limite;
+        public int Numero { get; set; }
+        public string Cliente { get; set; }
+        public string Cpf { get; set; }
+        public string Senha { get; set; }
+        public decimal Saldo { get; set; }
+        public decimal Limite { get; set; }
+
+        public decimal SaldoDisponivel => Saldo + Limite;
+
+        public Conta(int numero, string cliente, string cpf, string senha, decimal limite = 0)
+        {
+            Numero = numero;
+            Cliente = cliente;
+            Cpf = cpf;
+            Senha = senha;
+            Limite = limite;
+            Saldo = 0;
+        }
     }
 }
